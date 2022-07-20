1ce0ed6e369aa2c92017ba94a16258a14c927dce
aliqasim54321 <aliqasimkhundmiri10@gmail.com>
2022-07-20 13:08:41 +0530

    bitwise operator unary operator

---
+++ 3.js
@@ -0,0 +1,99 @@
+//Binary - 0bnnnn (2)
+// decimal - nnnn(10)
+// octal - 0onn (8)
+// hexadeciaml - 0xnn (16)
+
+//tostring
+// var a = 0b1000
+
+// console.log(a.toString(10)) //to convert to decimal
+// console.log(a.toString(2)) // to convert to binary
+// console.log(a.toString(8)) // to convert to octal
+// console.log(a.toString(16))// to convert to hexa decimal
+
+// //unary operator 
+// // +,-
+
+// console.log(1)
+// console.log(+1)
+// console.log(-1)
+
+// console.log('1')
+// console.log(+'1')
+// console.log(-'1')
+
+// console.log(+'t')
+// console.log(-'t')
+
+// //consoule.log (type of NaN)
+
+
+// var a= 1, b=1, c=true, d ='true'
+
+// console.log(a,typeof a)
+// console.log(b,typeof b)
+// console.log(c,typeof c)
+// console.log(d,typeof d)
+
+// console.log(+a,typeof +a)
+// console.log(+b,typeof +b)
+// console.log(+c,typeof +c)
+// console.log(+d,typeof +d)
+
+// console.log(-a,typeof -a)
+// console.log(-b,typeof -b)
+// console.log(-c,typeof -c)
+// console.log(-d,typeof -d)
+
+// Type casting: Converting one datatype to other
+// When the operand of an operator are different data types, one of them will be
+// converted to an 'equivalent' value of the other operands type.
+//*/
+
+
+// console.log(1+1+1+1)
+
+// console.log('1'+1)
+
+// console.log('1'+1+1+1)
+
+// console.log(1+'1'+1+1)
+
+// console.log(1+1+'1'+1)
+
+// console.log(1+1+1+'1')
+
+// console.log(false + false)
+// console.log(false + true)
+// console.log(false + 0)
+// console.log(1 + true)
+
+// console.log('hi code of india foundation')
+// console.log('hi', 'code of india' ,'foundation')
+// console.log('hi' +'code of india'+ 'foundation')
+
+// console.log('hi', 'code of india' ,'foundation', 2)
+// console.log('hi' +'code of india'+ 'foundation' + 2)
+
+// console.log(12+'hi')
+// console.log('12' + 'hi')
+// console.log('hi'+12)
+
+
+// console.log('100' + 1 + 2 + '3' + 4) 
+// console.log(100 + 1 + 2 + '3' + 4) 
+
+// console.log('1' + '2') //12
+// console.log('1' + 2) //12
+// console.log(1 + '2')  // 12
+// console.log(100 + 1 + '2')  // 1012
+// console.log('100' + 1 + '2')  // 10012
+// console.log('100' + 1 + 2)  // 10012
+
+
+// console.log('1' - '2')//-1
+// console.log('1' - 2)//-1
+// console.log(1 - '2')//-1
+// console.log(100 - 1 + '2')//992
+// console.log('100' - 1 - '2')//97
+// console.log('100' - 1 - 2)//97
